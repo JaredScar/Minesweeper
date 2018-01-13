07ed7d3181a5a4af91ceda45f7d55df432d6e28e
AHL96 <alexlopez841@gmail.com>
2018-01-13 11:55:41 -0500

    not much has changed. Mostly how im drawing the game

--- cell.js
+++ cell.js
@@ -1,66 +1,109 @@
-function Cell(i, j, size) {
-	this.i = i
-	this.j = j
-	this.bomb = false
-	this.flagged = false
-	this.number = 0
-	this.revealed = false
+function index(i, j) {
+  if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+    return -1;
+  }
 
-	this.x = i * size
-	this.y = j * size
-	this.size = size
+  return i + j * cols;
+}
 
-	this.neighbors = []
+function Cell(i, j, size, canvas) {
+  this.i = i
+  this.j = j
+  this.bomb = false
+  this.flagged = false
+  this.number = 0
+  this.revealed = false
 
-	this.show = (ctx) => {
-		var draw = new Draw(ctx)
-		draw.line(this.x, this.y, this.x + this.size, this.y + this.size)
-	}
+  this.x = i * size
+  this.y = j * size
+  this.size = size
 
-	this.neighborCount = (grid) => {
-		var total = 0
+  this.neighbors = []
 
-		// start in the top left and go clockwise
-		var is = [-1, -1, -1, 0, 1, 1, 1, 0]
-		var js = [-1, 0, 1, 1, 1, 0, -1, -1]
+  this.draw = new Shapes(canvas)
 
-		// console.log( "clicked on: " + this.i + ', ' + this.j )
+  this.show = () => {
 
-		for (var i = 0; i < 8; i++) {
-			var pos = grid[index(this.i + is[i], this.j + js[i])]
+    this.draw.square(this.x, this.y, this.size)
+    if (this.revealed) {
+      if (this.bomb) {
 
-			if (pos) {
-				this.neighbors.push(pos);
-				if (pos.bomb) {
-					total++
-				}
-			}
+        this.flagged = false
 
-			// console.log( pos )
+        this.draw.circle(
+          this.x + (this.size / 2),
+          this.y + (this.size / 2),
+          this.size / 4,
+          "red"
+        )
+      } else {
 
-			// console.log( "neighbor " + i + ': ' + pos.i + ', ' + pos.j )
-			// console.log( pos.bomb )
+        this.draw.square(this.x, this.y, this.size, "grey")
 
-		}
+        if (this.number !== 0) {
+          this.draw.text(
+            this.x + (this.size / 3),
+            this.y + (this.size / 1.5),
+            this.number
+          )
+        }
 
-		this.number = total
-		// console.log( this.number )
-	}
+      }
+    }
 
-	this.clicked = () => {
+    if (this.flagged) {
+      this.draw.text(
+        this.x + 10,
+        this.y + 25,
+        "?"
+      )
+    }
+  }
 
-	}
+  this.neighborCount = (grid) => {
+    var total = 0
 
-	this.reveal = () => {
-		this.revealed = true
-		if (!this.bomb && this.number === 0) {
-			for (let i = 0; i < this.neighbors.length; i++) {
-				this.neighbors[i].reveal()
-			}
-		}
-	}
+    // start in the top left and go clockwise
+    var is = [-1, -1, -1, 0, 1, 1, 1, 0]
+    var js = [-1, 0, 1, 1, 1, 0, -1, -1]
 
-	this.flag = () => {
-		this.flagged = !this.flagged
-	}
+    // console.log( "clicked on: " + this.i + ', ' + this.j )
+
+    for (var i = 0; i < 8; i++) {
+      var pos = grid[index(this.i + is[i], this.j + js[i])]
+
+      if (pos) {
+        this.neighbors.push(pos);
+        if (pos.bomb) {
+          total++
+        }
+      }
+
+      // console.log( pos )
+
+      // console.log( "neighbor " + i + ': ' + pos.i + ', ' + pos.j )
+      // console.log( pos.bomb )
+
+    }
+
+    this.number = total
+    // console.log( this.number )
+  }
+
+  this.clicked = (x, y) => {
+    return (
+      x > this.x &&
+      x < this.x + this.size &&
+      y > this.y &&
+      y < this.y + this.size
+    )
+  }
+
+  this.reveal = () => {
+    this.revealed = true;
+  }
+
+  this.flag = () => {
+    this.flagged = !this.flagged
+  }
 }
--- minesweeper.js
+++ minesweeper.js
@@ -1,26 +1,120 @@
 var grid = []
 var w = 30
+var gameOver = false
 
 var cols, rows
 
-var difficulty = 50;
+var difficulty = 50
+
+var bombLocs = []
+
+var canvas
+var ticker
+var draw
 
 
-function setup(ctx) {
-	var draw = new Draw(ctx)
+function setup(c) {
+  canvas = c
+  draw = new Shapes(canvas)
 
-	cols = Math.floor(ctx.width / w)
-	console.log(cols);
+  cols = Math.floor(draw.canvas.width / w)
+  rows = Math.floor(draw.canvas.height / w)
 
+  for (var j = 0; j < rows; j++) {
+    for (var i = 0; i < cols; i++) {
+      grid.push(new Cell(i, j, w, canvas))
+    }
+  }
 
+  for(var i = 0;i < difficulty; i++){
+    var r  = Math.floor(Math.random() * cols * rows)
+    //console.log(r)
+    if(grid[r].bomb === false){
+      grid[r].bomb = true;
+      bombLocs.push(r);
+    } else{
+      i--
+    }
+  }
+
+  for (var i = 0; i < grid.length; i++){
+    grid[i].neighborCount(grid)
+  }
+
+  //ticker = setInterval(update,1000)
+  update()
 }
 
 function update() {
+  console.log("tick")
+
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].show()
+  }
+
+  if (gameOver) {
+    for (var i = 0; i < grid.length; i++) {
+      if (grid[i].bomb && !hasWon()) {
+        grid[i].revealed = true;
+      }
+    }
+
+    setTimeout(clearInterval(ticker), 100)
+    console.log('ending game loop');
+  }
 
 
 }
 
+function mouseClick(event){
+  console.log("click")
 
-var canvas = document.getElementById("gameCanvas")
-var ctx = canvas.getContext("2d")
-setup(ctx)
+  var x = event.x
+  var y = event.y
+  console.log(x)
+  console.log(y)
+  console.log(draw.canvas.offSetLeft)
+  console.log(draw.canvas.offSetTop)
+
+  for (var i = 0; i < grid.length && !gameOver; i++) {
+    // console.log( grid[ i ] );
+
+
+
+    if (grid[i].clicked(x, y)) {
+
+      console.log( grid[ i ] );
+      /*
+          if cell with nothing around it is clicked
+          click on everything around it until a non-zero number is found
+          */
+
+      grid[i].reveal();
+
+      if (grid[i].bomb) {
+        gameOver = true;
+      } else if (hasWon()) {
+        winner();
+      }
+
+    }
+  }
+}
+
+function hasWon(){
+  return grid.every(function(item, index, arr){
+    if (item.bomb){
+      return !item.revealed
+    } else {
+      return item.revealed
+    }
+  })
+}
+
+function winner() {
+  setTimeout(function(){
+    alert("YOU WON")
+  }, 500)
+  gameOver = true
+}
+
